934cf0df4290e10c58338ce8056bbc00b30c9e8c
bearthesilly <2371478736@qq.com>
2026-04-18 11:18:37 +0800

    upload

--- scripts/long_term_forecast/AR_Baselines/run_all_ar_baselines.sh
+++ scripts/long_term_forecast/AR_Baselines/run_all_ar_baselines.sh
@@ -1,4 +1,5 @@
+export CUDA_VISIBLE_DEVICES=1
 #!/bin/bash
 set -euo pipefail
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-MODEL_LIST="AutoTimes DeepVAR GPVAR LSTM_AR TCN_AR LSTNet" bash "$script_dir/run_ar_baseline.sh"
+MODEL_LIST="AutoTimes DeepVAR LSTM_AR TCN_AR LSTNet" bash "$script_dir/run_ar_baseline.sh"
